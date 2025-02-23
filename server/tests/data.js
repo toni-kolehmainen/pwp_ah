65aa347b81530f4c09bda7a8d3e77eb86a9e182d
MOBUSSHAR ISLAM <mi.tonmoy4@gmail.com>
2025-02-23 16:11:44 +0200

    Initial test code updated

--- server/tests/data.js
+++ server/tests/data.js
@@ -5,9 +5,11 @@ const mockUser = {
   phone: '123-456-7890',
   password: 'hashedpassword123',
 }
+
 const mockUpdateUser = {
   password: 'hashedpassword123',
 }
+
 const mockUpdateUser1 = {
   name: 'pekka',
 }
@@ -24,24 +26,72 @@ const mockUserWrong = {
   password: 'hashedpassword123',
 }
 
-mockCategories = [
+const mockCategories = [
   {
     name: 'Toys',
-    description: 'lorum'
+    description: 'lorum',
   },
   {
-    name: 'Sports'
-  }
+    name: 'Sports',
+  },
 ]
-mockCategory = {
-  name: 'Electronics'
+
+const mockCategory = {
+  name: 'Electronics',
 }
-mockCategoryWrong = {
-  description: 'lorum'
+
+const mockCategoryWrong = {
+  description: 'lorum',
 }
-module.exports = { 
-  mockUser, mockCategory,
-  mockUserWrong, mockCategories, 
-  mockCategoryWrong, mockUpdateUser,
-  mockUpdateUserInvalid, mockUpdateUser1
-};
+
+// Mock data for items
+const mockItem = {
+  name: 'Sample Item',
+  description: 'This is a sample item.',
+  user_id: 1,
+  category_id: 1,
+}
+
+const mockItemWrong = {
+  description: 'This is a sample item without a name.',
+  user_id: 1,
+  category_id: 1,
+}
+
+const mockUpdateItem = {
+  description: 'This is an updated item description.',
+}
+
+const mockUpdateItem1 = {
+  name: 'Updated Item Name',
+}
+
+const mockUpdateItemInvalid = {
+  name: 'Updated Item Name',
+  description: 'This is an updated item description.',
+}
+
+const mockItems = [
+  {
+    id: 1,
+    name: 'Sample Item 1',
+    description: 'This is a sample item 1.',
+    user_id: 1,
+    category_id: 1,
+  },
+  {
+    id: 2,
+    name: 'Sample Item 2',
+    description: 'This is a sample item 2.',
+    user_id: 2,
+    category_id: 2,
+  },
+]
+
+module.exports = {
+  mockUser, mockCategory, mockUserWrong,
+  mockCategories, mockCategoryWrong, mockUpdateUser,
+  mockUpdateUserInvalid, mockUpdateUser1, mockItem,
+  mockItemWrong, mockUpdateItem, mockUpdateItemInvalid,
+  mockUpdateItem1, mockItems,
+}
