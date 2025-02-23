4d4e4319906bcbcb93432f868c422e18d56c19e9
MOBUSSHAR ISLAM <mi.tonmoy4@gmail.com>
2025-02-23 05:29:55 +0200

    Item crud updated

--- server/controllers/item.js
+++ server/controllers/item.js
@@ -1,53 +1,125 @@
-const { Item } = require('../models')
+const { Item } = require('../models');
+const Ajv = require('ajv');
+const { error } = require('../utils/logger');
+const ajv = new Ajv({ coerceTypes: false });
 
-const getItem = (req, res) => {
-  res.status(501).json({ message: 'This is not implemented' })
-  // Item.findOne({
-  //   where: {
-  //     id: req.params.id
-  //   }
-  // }).then((item) => {
-  //   res.json(item)
-  // }).catch((error) => {
-  //   return res.status(400).json({ error })
-  // })
-}
+const addSchema = {
+  type: "object",
+  properties: {
+    name: { type: "string" },
+    description: { type: "string" },
+    user_id: { type: "number" },
+    category_id: { type: "number" },
+  },
+  required: ["name", "description", "user_id", "category_id"],
+  additionalProperties: false
+};
 
-const addItem = (req, res) => {
-  res.status(501).json({ message: 'This is not implemented' })
-    // Item.create(req.body)
-    // .then(() => {
-    //   res.json({ status: 'Added' })
-    // })
-    // .catch((error) => {
-    //   return res.status(400).json({ error })
-    // })
-}
+const updateSchema = {
+  type: "object",
+  properties: {
+    name: { type: "string" },
+    description: { type: "string" },
+    user_id: { type: "number" },
+    category_id: { type: "number" },
+  },
+  minProperties: 1,
+  additionalProperties: false
+};
 
-const updateItem = (req, res) => {
-  res.status(501).json({ message: 'This is not implemented' })
-  // Item.update(req.body, {
-  //   where: {
-  //     id: req.params.id
-  //   }
-  // })
-}
+const getItem = async (req, res, next) => {
+  try {
+    const item = await Item.findOne({
+      where: {
+        id: req.params.id
+      }
+    });
 
-const deleteItem = (req, res) => {
-  res.status(501).json({ message: 'This is not implemented' })
-  // Item.destroy({
-  //   where: {
-  //     id: req.params.id
-  //   }
-  // })
-  // .then(() => {
-  //   res.json({ status: 'Deleted' })
-  // })
-}
+    if (!item) {
+      return res.status(404).json({ error: 'Item not found' });
+    }
+
+    res.json(item);
+  } catch (e) {
+    const error = new Error(e.message);
+    error.name = e.name;
+    return next(error);
+  }
+};
+
+const addItem = async (req, res, next) => {
+  const validate = ajv.compile(addSchema);
+  const valid = validate(req.body);
+
+  if (!valid) {
+    const error = new Error('Invalid Request body');
+    error.name = 'ValidationError';
+    return next(error);
+  }
+
+  try {
+    const item = await Item.create(req.body);
+    res.status(201).json(item);
+  } catch (e) {
+    const error = new Error(e.message);
+    error.name = e.name;
+    return next(error);
+  }
+};
+
+const updateItem = async (req, res, next) => {
+  const validate = ajv.compile(updateSchema);
+  const valid = validate(req.body);
+
+  if (!valid) {
+    const error = new Error('Invalid Request body');
+    error.name = 'ValidationError';
+    return next(error);
+  }
+
+  try {
+    const [updated] = await Item.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (updated) {
+      const updatedItem = await Item.findOne({ where: { id: req.params.id } });
+      return res.status(200).json(updatedItem);
+    }
+
+    throw new Error('Item not found');
+  } catch (e) {
+    const error = new Error(e.message);
+    error.name = e.name;
+    return next(error);
+  }
+};
+
+const deleteItem = async (req, res, next) => {
+  try {
+    const deleted = await Item.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (deleted) {
+      return res.status(204).send();
+    }
+
+    throw new Error('Item not found');
+  } catch (e) {
+    const error = new Error(e.message);
+    error.name = e.name;
+    return next(error);
+  }
+};
 
 module.exports = {
   getItem,
   addItem,
   updateItem,
   deleteItem
-}
+};
