b4c5a7c20cb47442e25a02907e081ee4a9720403
hrhm47 <haseebr470@gmail.com>
2025-02-27 20:14:28 +0200

    auction apis created

--- server/controllers/auctions.js
+++ server/controllers/auctions.js
@@ -1,13 +1,19 @@
 const { Auction } = require('../models');
+const Item = require('../models/item'); // Import the Item model
+const User = require('../models/user'); // Import the User model
+
 
 // GET /auctions: List all auctions
 
 const getAuctions = async (req, res, next) => {
   try {
     const auctions = await Auction.findAll({
-      attributes: ['id', 'desctription', 'end_time', 'starting_price', 'current_price'],
+      attributes: ['id', 'description', 'end_time', 'starting_price', 'current_price', 'user_id','item_id', 'seller_id'],
       order: [['end_time', 'ASC']] // Optional: Order by end_time ascending
     });
+    if (!auctions) {
+      return res.status(204).end();
+    }
     return res.json(auctions);
   } catch (e) {
     const error = new Error(e.message);
@@ -33,3 +39,4 @@ module.exports = {
   getAuctions,
   // deleteAuctions,
 };
+
