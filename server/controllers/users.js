bec7c3495ebf973eea406848bf980eaa519e7f5e
MiiroKuosmanen <32383287+MiiroKuosmanen@users.noreply.github.com>
2025-03-02 18:36:14 +0200

    users caching

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,19 +1,27 @@
-
-const { User } = require('../models');
+const { User } = require("../models");
 
 // hypermedia
 // to auction, to bids, to items?
 
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
+
 const getUsers = async (req, res) => {
+  const cacheKey = "users";
+  const cachedUsers = myCache.get(cacheKey);
+  if (cachedUsers) {
+    return res.json(cachedUsers);
+  }
   const users = await User.findAll({
-    attributes: { exclude: ['password'] }
-  })
+    attributes: { exclude: ["password"] },
+  });
   if (users.length === 0) {
     return res.status(204).end();
   }
+  myCache.set(cacheKey, users);
   return res.json(users);
 };
 
 module.exports = {
-  getUsers
+  getUsers,
 };
