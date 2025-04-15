dac1e452ea1505278ca4c398a8081950303e00a9
MOBUSSHAR ISLAM <mi.tonmoy4@gmail.com>
2025-04-15 22:43:07 +0300

    Swagger Document Distributed

--- server/utils/swagger_setup.js
+++ server/utils/swagger_setup.js
@@ -2,7 +2,7 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
 
-const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger.yaml'));
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger-bundled.yaml'));
 
 const setupSwagger = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
